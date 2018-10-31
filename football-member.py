3f1ca588c83483282d78ece96019c7ef7672fcf9
chucw <chucw@users.noreply.github.com>
2018-11-01 02:43:48 +0900

    football-list 신규 작성
    플레이어들을 DB에 저장하기 위한 사전 작업

--- football-member.py
+++ football-member.py
@@ -11,12 +11,39 @@ import sys
 url = "https://sportsdata.pstatic.net/ndata//kleague/2018/10/201810061718188.json"
 html = rq.get(url).text
 
-matched = re.search(r'"goalInfo":(.*)', html)
+data = json.loads(html)
 
-tt = matched.group(1)
-tt = tt[:-1]
+num = len(data["lineup"]["away"])
+num2 = len(data["lineup"]["home"])
 
-data = json.loads(tt)
+for i in range(0, num):
+    if data["lineup"]["away"][i]["posOrder"] == "-1":
+        pass
+    else:
+        print (data["gameInfo"]["dateTime"], end='')
+        print(',', end='')
+        print("인천 : ", end='')
+        print(data["lineup"]["away"][i]["pName"], end='')               #플레이어 이름
+        print(',', end='')
+        print(data["lineup"]["away"][i]["pos"], end='')                 #포지션
+        print(',', end='')
+        print(data["lineup"]["away"][i]["posOrder"], end='')            #수비위치
+        print(',', end='')
+        if 'sTime' in data["lineup"]["away"][i]:
+            if data["lineup"]["away"][i]["sType"] == "in":
+                stime = 90 - int(data["lineup"]["away"][i]["sTime"])
+                print(stime, end='')
+                print(',', end='')                                            #출전시간
+            else:
+                print(data["lineup"]["away"][i]["sTime"], end='')
+                print(',', end='')                                            #출전시간
+        else:
+            print("90", end='')
+            print(',', end='')                                            #출전시간  
+        print(data["lineup"]["away"][i]["goal"])  
 
-
-print (data)
+print("=========================================")
+'''
+for j in range(0, num):
+    print(data["lineup"]["home"][j]["pName"])    
+'''
--- insert-football-2.py
+++ insert-football-2.py
@@ -7,19 +7,21 @@ import pandas
 import pymysql
 import sys
 
-url = "https://sports.news.naver.com/wfootball/schedule/index.nhn?category=europa&year=2018&month=10"
-html = rq.get(url).text
+url_2 = "http://sports.news.naver.com/gameCenter/textRelayFootball.nhn?category=kleague&gameId=201810061718188"
+html_2 = rq.get(url_2).text
 
 # print (html)
 
 #matched = re.search(r'dailyScheduleListMap":(.*?;)', html, re.S)
-matched = re.search(r'monthlyScheduleModel:(.*)', html)
+matched_2 = re.search(r'relayDataUrl:(.*)', html_2)
 
-tt = matched.group(1)
-tt = tt[:-1]
+tt_2 = matched_2.group(1)
+#tt_2 = tt_2[:-1]
+print(tt_2)
 
-data = json.loads(tt)
+#data = json.loads(tt_2)
 
-aaa = data["dailyScheduleListMap"]
 
-print(tt)
+##aaa = data['relayDataUrl']
+
+#print(aaa)
