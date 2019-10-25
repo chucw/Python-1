6326ec8a5bb3025e83c2d5a8153364797126600a
chucw <34207606+chucw@users.noreply.github.com>
2019-10-25 22:29:16 +0900

    2019년 적용

--- insert-kbl-basketball.py
+++ insert-kbl-basketball.py
@@ -1,21 +1,20 @@
-#-*- coding: utf-8 -*
+# -*- coding: utf-8 -*
 import requests as rq
 from bs4 import BeautifulSoup
 import pymysql
 
-url = "https://sports.news.naver.com/basketball/schedule/index.nhn?category=kbl&year=2018&month=11"
+url = "https://sports.news.naver.com/basketball/schedule/index.nhn?category=kbl&year=2019&month=10"
 html = rq.get(url).text
 soup = BeautifulSoup(html, 'html.parser')
 
 t_div = soup.find_all("tr")
 
-conn = pymysql.connect(host='localhost', port=3306,  db='betman', user='root', password='syjm1998')
+conn = pymysql.connect(host='localhost', port=3306, db='betman', user='root', password='syjm1998')
 curs = conn.cursor()
 
-
 for i in range(len(t_div)):
-    if t_div[i].find('span',{'class':'td_date'}):
-        td_date = t_div[i].find('span',{'class':'td_date'}).text
+    if t_div[i].find('span', {'class': 'td_date'}):
+        td_date = t_div[i].find('span', {'class': 'td_date'}).text
 
         a = len(td_date)
         b = td_date.find('.')
@@ -25,46 +24,64 @@ for i in range(len(t_div)):
         elif a == 8:
             if b == 1:
                 month = '0' + td_date[0:b]
-                dat = td_date[b+1:b+3]
+                dat = td_date[b + 1:b + 3]
             else:
                 month = td_date[0:b]
-                dat = '0' + td_date[b+1:b+2]
+                dat = '0' + td_date[b + 1:b + 2]
         else:
             month = '0' + td_date[0:b]
-            dat = dat = '0' + td_date[b+1]
+            dat = dat = '0' + td_date[b + 1]
 
-    
-    if t_div[i].find('span',{'class':'td_hour'}):
-        td_hour = t_div[i].find('span',{'class':'td_hour'}).text
+    if t_div[i].find('span', {'class': 'td_hour'}):
+        td_hour = t_div[i].find('span', {'class': 'td_hour'}).text
         if td_hour == '-':
             pass
         else:
-            game_date = '2018' + month + dat
-            hometeam = t_div[i].find('span',{'class':'team_lft'}).text
-            score = t_div[i].find('strong',{'class':'td_score'}).text
+            game_date = '2019' + month + dat
+            hometeam = t_div[i].find('span', {'class': 'team_lft'}).text
+            score = t_div[i].find('strong', {'class': 'td_score'}).text
             s_len = len(score)
             s_find = score.find(':')
             home_score = score[0:s_find]
-            away_score = score[s_find+1:]
-            awayteam = t_div[i].find('span',{'class':'team_rgt'}).text
-           
-            print(game_date, end=' ')
-            print(hometeam, end=' ')
-            print(home_score, end=' ')
-            print(away_score, end=' ')
-            print(awayteam)
+            away_score = score[s_find + 1:]
+            awayteam = t_div[i].find('span', {'class': 'team_rgt'}).text
 
-            if game_date >= '20181128' and game_date < '20181130':
-                sql = "INSERT INTO `basketball` ( `game_date`, `homeTeam`, `homeScore`, \
-                `awayTeam`, `awayScore`, `league`) VALUES "
-                sql = sql + '(' + "'" + str(game_date) + "','" + str(hometeam) + "'," \
-                + str(home_score) + ",'" + str(awayteam) + "', " + str(away_score) + ", 'KBL');"
+            #            print(game_date, end=' ')
+            #            print(hometeam, end=' ')
+            #            print(home_score, end=' ')
+            #            print(away_score, end=' ')
+            #            print(awayteam)
+
+            if home_score == 'V':
+                pass
+            else:
+
+                sql = "SELECT COUNT(*) FROM football WHERE game_date = '" + str(game_date) + "'"
+                sql = sql + " and homeTeam = '" + str(hometeam) + "'"
+                sql = sql + " and awayTeam = '" + str(awayteam) + "'"
+
                 curs.execute(sql)
 
-    else:
-        pass
+                result = curs.fetchall()
+                for row_data in result:
+                    if row_data[0] == 0:
+                        sql = "INSERT INTO `basketball` ( `game_date`, `homeTeam`, `homeScore`, \
+                                        `awayTeam`, `awayScore`, `league`) VALUES "
+                        sql = sql + '(' + "'" + str(game_date) + "','" + str(hometeam) + "'," \
+                              + home_score + ",'" + str(awayteam) + "', " + away_score + ", 'KBL');"
+                        curs.execute(sql)
+                    #                    print(sql)
+                    else:
+                        sql = "UPDATE `basketball` SET "
+                        sql = sql + "homeScore = " + home_score
+                        sql = sql + ", awayScore = " + away_score
+                        sql = sql + " where game_date = '" + str(game_date) + "'"
+                        sql = sql + " and homeTeam = '" + str(hometeam) + "'"
+                        sql = sql + " and awayTeam = '" + str(awayteam) + "'"
+                        sql = sql + " and league = 'KBL'"
+                        curs.execute(sql)
 
-conn.commit()   
-conn.close()         
+conn.commit()
+conn.close()
 
-print ("OK")        
+print("OK")
--- temp.py
+++ temp.py
@@ -1,58 +1,56 @@
-#-*- coding: utf-8 -*
-import re
+# -*- coding: utf-8 -*
 import requests as rq
-import json
-import calendar
-import pandas
-import sys
+from bs4 import BeautifulSoup
+import pymysql
 
-
-url = "https://sports.news.naver.com/wfootball/schedule/index.nhn?category=epl&year=2019&month=10"
+url = "https://sports.news.naver.com/basketball/schedule/index.nhn?category=kbl&year=2019&month=10"
 html = rq.get(url).text
+soup = BeautifulSoup(html, 'html.parser')
 
-#print (html)
+t_div = soup.find_all("tr")
 
-#matched = re.search(r'dailyScheduleListMap":(.*?;)', html, re.S)
+conn = pymysql.connect(host='localhost', port=3306, db='betman', user='root', password='syjm1998')
+curs = conn.cursor()
 
-#matched = re.search(r'scheduleList:(.*)', html)
-matched = re.search(r'monthlyScheduleDailyGroup":(.*)', html, re.S)
+for i in range(len(t_div)):
+    if t_div[i].find('span', {'class': 'td_date'}):
+        td_date = t_div[i].find('span', {'class': 'td_date'}).text
 
-tt = matched.group(1)
-
-c1 = tt.find("monthList")
-#print(c1)
-
-src = tt[:c1-2]
-
-#print(src)
-
-
-
-for i in range(0, 1):
-#    print(i)
-#    print(json.loads(src)[i])
-    src1 = json.loads(src)[i]
-#    print(src1['date'])
-#    print(src1['scheduleList'])
-    s_empty = str(src1['empty'])
-#    s_empty = s_empty.strip()
-
-    if s_empty != 'true':
-#        print(src1['date'])
-#        print(src1['scheduleList'])
-
-
-        obj = src1['scheduleList']
-        for j in range(0, len(obj)):
-            if obj[j]['homeTeamName'] is None:
-                pass
+        a = len(td_date)
+        b = td_date.find('.')
+        if a == 9:
+            month = td_date[0:2]
+            dat = td_date[3:5]
+        elif a == 8:
+            if b == 1:
+                month = '0' + td_date[0:b]
+                dat = td_date[b + 1:b + 3]
             else:
-                print(src1['date'])
-                print(obj[j]['homeTeamName'])
-                print(obj[j]['awayTeamName'])
-                print(obj[j]['homeTeamScore'])
-                print(obj[j]['awayTeamScore'])
+                month = td_date[0:b]
+                dat = '0' + td_date[b + 1:b + 2]
+        else:
+            month = '0' + td_date[0:b]
+            dat = dat = '0' + td_date[b + 1]
+
+    if t_div[i].find('span', {'class': 'td_hour'}):
+        td_hour = t_div[i].find('span', {'class': 'td_hour'}).text
+        if td_hour == '-':
+            pass
+        else:
+            game_date = '2019' + month + dat
+            hometeam = t_div[i].find('span', {'class': 'team_lft'}).text
+            score = t_div[i].find('strong', {'class': 'td_score'}).text
+            s_len = len(score)
+            s_find = score.find(':')
+            home_score = score[0:s_find]
+            away_score = score[s_find + 1:]
+            awayteam = t_div[i].find('span', {'class': 'team_rgt'}).text
+
+            print(game_date, end=' ')
+            print(hometeam, end=' ')
+            print(home_score, end=' ')
+            print(away_score, end=' ')
+            print(awayteam)
 
 
-
-print('OK')
+print("OK")
