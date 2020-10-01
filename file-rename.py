718de9ee8ba7112f4b75e7d5192faf41979e121d
chucw <chucw@naver.com>
2020-10-02 06:25:47 +0900

    대상 경로 변경

--- file-rename.py
+++ file-rename.py
@@ -1,6 +1,6 @@
 import os
 
-file_path = 'E:\\practice\\Data Science\\Kaggle 파이썬을 활용한 머신러닝 실전 예제 분석'
+file_path = 'E:\\practice\\Data Science\\파이썬 기반의 AI를 위한 기초수학, 확률 및 통계'
 file_names = os.listdir(file_path)
 
 i = 1
