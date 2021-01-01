bceefa392877b55cb5d77aff178567649fe0bf48
chucw <chucw@bizwizsystem.co.kr>
2021-01-01 22:56:21 +0900

    요식 아마존 업로드

---
+++ starpos/aws-upload-food.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*
+import pymysql
+
+cus_cd = "FCEB6AFC"   #명륜진사갈비
+
+conn = pymysql.connect(host='119.194.242.212', port=3306, db='starpos_r', user='root', password='syjm1998')
+curs = conn.cursor()
+
+conn2 = pymysql.connect(host='starpos-production-instance.ch1gq016d78x.ap-northeast-1.rds.amazonaws.com', port=3306, db='starpos', user='starpos', password='starpos2714')
+curs2 = conn2.cursor()
+
+sql = " select * from tb_payment where payment_id > 1956 "
+curs.execute(sql)
+
+result = curs.fetchall()
+
+for row_data in result:
+#    print(row_data[0])
+#    print(row_data[1])
+#    print(row_data[2])
+#    sql2 = "INSERT INTO `trandeta` ( `cus_cd`, `branch_cd`, `posno`, `ydate`, `recno`) values "
+#    sql2 = sql2 + " ('"+cus_cd+"', '"+str(row_data[1])+"', '"+str(row_data[2])+"', '"+str(row_data[3])+"', "+str(row_data[4])+"); "
+
+    sql2 = "INSERT INTO `tb_cus_branch_payment` ( `payment_id`, `branch_cd`, `cus_cd`, `operation_id`, `table_id`, \
+		`status`, `credit_card`, `change`, `cash`, `point`, \
+        `total`, `receipt_info`, `created_dt`, `mod_dt`, `reg_dt`) values \
+		("+str(row_data[0])+", '"+str(row_data[1])+"', '"+cus_cd+"', "+str(row_data[2])+", "+str(row_data[3])+", \
+		 "+str(row_data[4])+",  "+str(row_data[5])+", "+str(row_data[6])+", "+str(row_data[7])+", "+str(row_data[8])+", \
+		 "+str(row_data[9])+", '"+str(row_data[10])+"', "+str(row_data[11])+", "+UNIX_TIMESTAMP(str(row_data[12]))+", NOW()  ); "
+
+    curs2.execute(sql2)
+    print(sql2)
+
+conn2.commit()
+
+conn.close()
+conn2.close()
+
+print("OK")
