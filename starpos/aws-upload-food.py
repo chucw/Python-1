1b392afb85e5d4ee0ff6bee4a21ebedffb8936ef
chucw <chucw@bizwizsystem.co.kr>
2021-01-02 18:26:27 +0900

    요식 아마존 업로드 소스 수정

--- starpos/aws-upload-food.py
+++ starpos/aws-upload-food.py
@@ -9,8 +9,13 @@ curs = conn.cursor()
 conn2 = pymysql.connect(host='starpos-production-instance.ch1gq016d78x.ap-northeast-1.rds.amazonaws.com', port=3306, db='starpos', user='starpos', password='starpos2714')
 curs2 = conn2.cursor()
 
-sql = " select * from tb_payment where payment_id > 1956 "
+sql = " select `payment_id`, `branch_id`, `operation_id`, `table_id`, \
+        `status`, `credit_card`, `change`, `cash`, `point`, \
+        `total`, Replace(`receipt_info`, \"\'\", '""'), `created_at` \
+        from tb_payment where payment_id BETWEEN 17761 AND 17883 "
+print(sql) 
 curs.execute(sql)
+
 
 result = curs.fetchall()
 
@@ -18,18 +23,18 @@ for row_data in result:
 #    print(row_data[0])
 #    print(row_data[1])
 #    print(row_data[2])
-#    sql2 = "INSERT INTO `trandeta` ( `cus_cd`, `branch_cd`, `posno`, `ydate`, `recno`) values "
-#    sql2 = sql2 + " ('"+cus_cd+"', '"+str(row_data[1])+"', '"+str(row_data[2])+"', '"+str(row_data[3])+"', "+str(row_data[4])+"); "
+#    sql2 = "INSERT INTO `tb_cus_branch_payment` ( `payment_id`, `branch_cd`, `cus_cd`, `operation_id`, `table_id`) values \
+#            ("+str(row_data[0])+", '"+str(row_data[1])+"', '"+cus_cd+"', "+str(row_data[2])+", "+str(row_data[3])+" ); "
+           
 
     sql2 = "INSERT INTO `tb_cus_branch_payment` ( `payment_id`, `branch_cd`, `cus_cd`, `operation_id`, `table_id`, \
 		`status`, `credit_card`, `change`, `cash`, `point`, \
         `total`, `receipt_info`, `created_dt`, `mod_dt`, `reg_dt`) values \
 		("+str(row_data[0])+", '"+str(row_data[1])+"', '"+cus_cd+"', "+str(row_data[2])+", "+str(row_data[3])+", \
 		 "+str(row_data[4])+",  "+str(row_data[5])+", "+str(row_data[6])+", "+str(row_data[7])+", "+str(row_data[8])+", \
-		 "+str(row_data[9])+", '"+str(row_data[10])+"', "+str(row_data[11])+", UNIX_TIMESTAMP("+str(row_data[12])+"), NOW()  ); "
-
+		 "+str(row_data[9])+", '"+str(row_data[10])+"', "+str(row_data[11])+", NOW(), NOW()  ); "
     curs2.execute(sql2)
-    print(sql2)
+    print(sql2) 
 
 conn2.commit()
 
