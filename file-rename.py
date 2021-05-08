c0b389406e18192c2ac9b471f04cfff681394f23
chucw <chucw@bizwizsystem.co.kr>
2021-05-08 22:54:05 +0900

    모두 적용

--- file-rename.py
+++ file-rename.py
@@ -1,7 +1,7 @@
 import os
 
-#file_path = 'E:\\practice\\인프라-네트웍\\네트워크 엔지니어의 교과서'
-file_path = 'D:\\temp\\Language\\Python\\파이썬을 활용한 네트워크프로그래밍'
+file_path = 'E:\\practice\\Language\\스프링5와 Vue-js로 시작하는 모던 웹 애플리케이션 개발'
+#file_path = 'D:\\temp\\System\\\\Docker'
 file_names = os.listdir(file_path)
 
 i = 1
